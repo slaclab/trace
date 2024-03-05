b5c552a3bf5c48cfe2aad95ff14bb67bd004b3d7
Zach Domke <zdomke@slac.stanford.edu>
2024-03-05 15:49:59 -0800

    Changes to main splitter

--- archive_viewer/archive_viewer.py
+++ archive_viewer/archive_viewer.py
@@ -5,10 +5,12 @@ from mixins import (TracesTableMixin, AxisTableMixin, ArchiversTabMixin)
 
 
 class ArchiveViewer(Display, TracesTableMixin, AxisTableMixin, ArchiversTabMixin):
+    def __init__(self, parent=None, args=None, macros=None, ui_filename=__file__.replace(".py", ".ui")):
         super(ArchiveViewer, self).__init__(parent=parent, args=args,
                                             macros=macros, ui_filename=ui_filename)
 
-        # self.ui.archiver_select_cmbx.addItems(archiver_urls)
+        self.ui.main_spltr.setCollapsible(0, False)
+        self.ui.main_spltr.setStretchFactor(0, 1)
 
         self.axis_table_init()
         self.traces_table_init()
