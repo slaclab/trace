6286d99bbd125ea3bb772583be60f6a14a5d2ff1
Zach Domke <zdomke@slac.stanford.edu>
2024-05-06 11:12:48 -0700

    ENH: Add slots & connections for setting x-axis

--- archive_viewer/mixins/axis_table.py
+++ archive_viewer/mixins/axis_table.py
@@ -1,5 +1,8 @@
-from qtpy.QtCore import Slot
+from typing import Tuple
+from datetime import datetime
+from qtpy.QtCore import Slot, QDateTime
 from qtpy.QtWidgets import QHeaderView
+from pyqtgraph import ViewBox
 from table_models import ArchiverAxisModel
 from widgets import ComboBoxDelegate, ScientificNotationDelegate
 
@@ -19,6 +22,13 @@ class AxisTableMixin:
         self.ui.add_axis_row_btn.clicked.connect(self.addAxis)
         self.ui.del_axis_row_btn.clicked.connect(self.removeSelectedAxis)
 
+        plot_viewbox = self.ui.archiver_plot.plotItem.vb
+        plot_viewbox.sigXRangeChanged.connect(self.set_axis_datetimes)
+        plot_viewbox.sigRangeChangedManually.connect(lambda *_: self.set_axis_datetimes())
+
+        self.ui.main_start_datetime.dateTimeChanged.connect(lambda qdt: self.set_time_axis_range((qdt, None)))
+        self.ui.main_end_datetime.dateTimeChanged.connect(lambda qdt: self.set_time_axis_range((None, qdt)))
+
     def axis_delegates_init(self) -> None:
         """Initialize and set the ItemDelegates for the axis table."""
         orientation_col = self.axis_table_model.getColumnIndex("Y-Axis Orientation")
@@ -33,6 +43,57 @@ class AxisTableMixin:
         max_range_col = self.axis_table_model.getColumnIndex("Max Y Range")
         max_range_del = ScientificNotationDelegate(self.ui.time_axis_tbl)
         self.ui.time_axis_tbl.setItemDelegateForColumn(max_range_col, max_range_del)
+
+    Slot(object)
+    def set_time_axis_range(self, raw_range: Tuple[QDateTime, QDateTime] = (None, None)) -> None:
+        """PyQT Slot to set the plot's X-Axis range. This slot should be
+        triggered on QDateTimeEdit value change.
+
+        Parameters
+        ----------
+        raw_range : Tuple[QDateTime], optional
+            Takes in a tuple of 2 values, where one is a QDateTime and
+            the other is None. The positioning changes either the plot's
+            min or max range value. By default (None, None)
+        """
+        proc_range = [None, None]
+        for ind, val in enumerate(raw_range):
+            # Values that are QDateTime are converted to a float timestamp
+            if isinstance(val, QDateTime):
+                proc_range[ind] = val.toSecsSinceEpoch()
+            # Values that are None use the existing range value
+            elif not val:
+                proc_range[ind] = self.ui.archiver_plot.getXAxis().range[ind]
+        proc_range.sort()
+
+        self.ui.archiver_plot.plotItem.vb.blockSignals(True)
+        self.ui.archiver_plot.plotItem.setXRange(*raw_range)
+        self.ui.archiver_plot.plotItem.vb.blockSignals(False)
+
+    @Slot(object, object)
+    def set_axis_datetimes(self, _: ViewBox = None, time_range: Tuple[float, float] = None) -> None:
+        """Slot used to update the QDateTimeEdits on the Axis tab. This
+        slot is called when the plot's X-Axis range changes values.
+
+        Parameters
+        ----------
+        _ : ViewBox, optional
+            The ViewBox on which the range is changing. This is unused
+        time_range : Tuple[float, float], optional
+            The new range values for the QDateTimeEdits, by default None
+        """
+        if not time_range:
+            time_range = self.ui.archiver_plot.getXAxis().range
+        if min(time_range) <= 0:
+            return
+
+        time_range = [datetime.fromtimestamp(f) for f in time_range]
+
+        edits = (self.ui.main_start_datetime, self.ui.main_end_datetime)
+        for ind, qdt in enumerate(edits):
+            qdt.blockSignals(True)
+            qdt.setDateTime(QDateTime(time_range[ind]))
+            qdt.blockSignals(False)
 
     @Slot()
     def addAxis(self) -> None:
