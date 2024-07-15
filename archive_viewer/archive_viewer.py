85287b7860bf04496538718ed533e73b485c4289
Akshar Sarvesh <aksharsarvesh@gmail.com>
2024-07-15 11:39:33 -0700

    Worked at a few changes for  precommit

--- archive_viewer/archive_viewer.py
+++ archive_viewer/archive_viewer.py
@@ -3,14 +3,13 @@ from qtpy.QtCore import Slot
 from qtpy.QtWidgets import QAbstractButton, QApplication
 from pydm import Display
 from config import logger
-from mixins import (TracesTableMixin, AxisTableMixin)
+from mixins import TracesTableMixin, AxisTableMixin
 from styles import CenterCheckStyle
 
 
 class ArchiveViewer(Display, TracesTableMixin, AxisTableMixin):
     def __init__(self, parent=None, args=None, macros=None, ui_filename=__file__.replace(".py", ".ui")) -> None:
-        super(ArchiveViewer, self).__init__(parent=parent, args=args,
-                                            macros=macros, ui_filename=ui_filename)
+        super(ArchiveViewer, self).__init__(parent=parent, args=args, macros=macros, ui_filename=ui_filename)
 
         self.ui.main_spltr.setCollapsible(0, False)
         self.ui.main_spltr.setStretchFactor(0, 1)
@@ -21,12 +20,14 @@ class ArchiveViewer(Display, TracesTableMixin, AxisTableMixin):
         self.curve_delegates_init()
         self.axis_delegates_init()
 
-        self.button_spans = {self.ui.half_min_scale_btn: 30,
-                             self.ui.min_scale_btn: 60,
-                             self.ui.hour_scale_btn: 3600,
-                             self.ui.week_scale_btn: 604800,
-                             self.ui.month_scale_btn: 2628300,
-                             self.ui.cursor_scale_btn: -1}
+        self.button_spans = {
+            self.ui.half_min_scale_btn: 30,
+            self.ui.min_scale_btn: 60,
+            self.ui.hour_scale_btn: 3600,
+            self.ui.week_scale_btn: 604800,
+            self.ui.month_scale_btn: 2628300,
+            self.ui.cursor_scale_btn: -1
+        }
         self.ui.timespan_btns.buttonClicked.connect(partial(self.set_plot_timerange))
 
         plot_viewbox = self.ui.archiver_plot.plotItem.vb
@@ -52,7 +53,7 @@ class ArchiveViewer(Display, TracesTableMixin, AxisTableMixin):
             logger.error(f"{button} is not a valid timespan button")
             return
 
-        enable_scroll = (button != self.ui.cursor_scale_btn)
+        enable_scroll = button != self.ui.cursor_scale_btn
         timespan = self.button_spans[button]
 
         self.ui.archiver_plot.setAutoScroll(enable_scroll, timespan)
