4f3e594c99e22e16db7f637ae261ab5f3f1ad22d
Zach Domke <zdomke@slac.stanford.edu>
2024-07-31 13:46:01 -0700

    ENH: Add argument parsing to launch script

--- launch_archive_viewer.bash
+++ launch_archive_viewer.bash
@@ -12,7 +12,24 @@ exit_abnormal(){
     exit 1
 }
 
+MACROS=""
+IMPORT_FILE=""
+
+while [ $# -gt 0 ]
+do
+    case $1 in
+        -m | --macros) MACROS="$2"
+                       shift ;;
+        -i | --import) IMPORT_FILE="$2"
+                       shift ;;
+        -h | --help) exit_abnormal ;;
+        *) exit_abnormal
+    esac
+    shift
+done
+
 pydm --hide-nav-bar --hide-status-bar \
+    -m "MACROS" \
     archive_viewer.py
 
 exit 0
