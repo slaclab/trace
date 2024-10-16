24b0516dbb93046bc0c5f1743c7ef7c2e5858d69
Zach Domke <zdomke@slac.stanford.edu>
2024-10-16 15:23:46 -0700

    TST: Add plot config tests

--- trace/tests/test_mixins/test_plot_config.py
+++ trace/tests/test_mixins/test_plot_config.py
@@ -1,37 +1,47 @@
-from main import TraceDisplay
+from json import loads
+
+import pytest
+from pyqtgraph import ViewBox
 
 
-def test_set_plot_title(qtrace: TraceDisplay):
+def test_plot_setup(qtrace, get_test_file):
+    test_file = get_test_file("test_file.trc")
+    test_data = loads(test_file.read_text())["plot"]
+
+    qtrace.plot_setup(test_data)
+
+    assert qtrace.plot.to_dict() == test_data
+
+
+@pytest.mark.parametrize("test_size, expected_size", ((-5, 1), (10, 10), (200, 99)))
+def test_set_font_size(qtrace, test_size, expected_size):
+    qtrace.ui.xafs_spnbx.setValue(test_size)
+    x_axis = qtrace.plot.getAxis("bottom")
+
+    assert x_axis.style["tickFont"].pixelSize() == expected_size
+
+
+@pytest.mark.parametrize(
+    "test_index, expected_value", ((0, ViewBox.RectMode), (1, ViewBox.PanMode), (3, ViewBox.RectMode))
+)
+def test_change_mouse_mode(qtrace, test_index, expected_value):
+    qtrace.ui.mouse_mode_cmbbx.setCurrentIndex(test_index)
+
+    vb = qtrace.plot.plotItem.getViewBox()
+    assert vb.state["mouseMode"] == expected_value
+
+
+def test_auto_scroll(qtrace):
     pass
 
 
-def test_set_gridlines(qtrace: TraceDisplay):
+def test_change_opacity(qtrace):
     pass
 
 
-def test_set_legend(qtrace: TraceDisplay):
+def test_show_x_grid(qtrace):
     pass
 
 
-def test_set_x_axis_font_size(qtrace: TraceDisplay):
-    pass
-
-
-def test_set_refresh_interval(qtrace: TraceDisplay):
-    pass
-
-
-def test_set_background_color(qtrace: TraceDisplay):
-    pass
-
-
-def test_set_mouse_mode(qtrace: TraceDisplay):
-    pass
-
-
-def test_set_opacity(qtrace: TraceDisplay):
-    pass
-
-
-def test_set_crosshair(qtrace: TraceDisplay):
+def test_show_y_grid(qtrace):
     pass
