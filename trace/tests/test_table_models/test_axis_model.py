21ceea58fcecad1bd9a5d3582f981838cd3c98b8
Zach Domke <zdomke@slac.stanford.edu>
2024-10-24 15:15:08 -0700

    TST: Add qtmodeltester for table models

--- trace/tests/test_table_models/test_axis_model.py
+++ trace/tests/test_table_models/test_axis_model.py
@@ -1,9 +1,24 @@
 from qtpy.QtCore import Qt
 
-from main import TraceDisplay
+
+def test_axis_table_model_qtmodeltester(qtmodeltester, qtrace):
+    """Check the validity of the ArchiverAxisModel with pytest-qt
+
+    Parameters
+    ----------
+    qtmodeltester : fixture
+        pytest-qt fixture used for testing the validity of AbstractItemModels
+    qtrace : fixture
+        Instance of TraceDisplay for application testing
+
+    Expectations
+    ------------
+    qtmodeltester finds no issues with the model
+    """
+    qtmodeltester.check(qtrace.axis_table_model, force_py=True)
 
 
-def test_default_axis(qtrace: TraceDisplay):
+def test_default_axis(qtrace):
     """Check default values for axis as represented in the table model"""
     axis_model = qtrace.axis_table_model
 
@@ -19,17 +34,17 @@ def test_default_axis(qtrace: TraceDisplay):
     assert row_actual == row_expected
 
 
-def test_append_axis(qtrace: TraceDisplay):
+def test_append_axis(qtrace):
     pass
 
 
-def test_remove_axis(qtrace: TraceDisplay):
+def test_remove_axis(qtrace):
     pass
 
 
-def test_set_model_axes(qtrace: TraceDisplay):
+def test_set_model_axes(qtrace):
     pass
 
 
-def test_alter_axis_data(qtrace: TraceDisplay):
+def test_alter_axis_data(qtrace):
     pass
--- trace/tests/test_table_models/test_curve_model.py
+++ trace/tests/test_table_models/test_curve_model.py
@@ -1,14 +1,29 @@
-from qtpy.QtCore import Qt, QVariant
-
-from main import TraceDisplay
+from qtpy.QtCore import Qt
 
 
-def test_default_curve(qtrace: TraceDisplay):
+def test_curves_model_qtmodeltester(qtmodeltester, qtrace):
+    """Check the validity of the ArchiverCurveModel with pytest-qt
+
+    Parameters
+    ----------
+    qtmodeltester : fixture
+        pytest-qt fixture used for testing the validity of AbstractItemModels
+    qtrace : fixture
+        Instance of TraceDisplay for application testing
+
+    Expectations
+    ------------
+    qtmodeltester finds no issues with the model
+    """
+    qtmodeltester.check(qtrace.curves_model, force_py=True)
+
+
+def test_default_curve(qtrace):
     """Check default values for curve as represented in the table model"""
     curves_model = qtrace.curves_model
 
     row_actual = []
-    row_expected = [QVariant(), Qt.Checked, Qt.Checked, "", "#008cf9", "Axis 1",
+    row_expected = ["", Qt.Checked, Qt.Checked, "", "#008cf9", "Axis 1",
                     "Direct", "Solid", "1px", "None", "10px", Qt.Checked, None]  # fmt: skip
 
     for col in range(curves_model.columnCount()):
@@ -20,55 +35,55 @@ def test_default_curve(qtrace: TraceDisplay):
     assert row_actual == row_expected
 
 
-def test_contains(qtrace: TraceDisplay):
+def test_contains(qtrace):
     pass
 
 
-def test_append_curve(qtrace: TraceDisplay):
+def test_append_curve(qtrace):
     pass
 
 
-def test_remove_curve(qtrace: TraceDisplay):
+def test_remove_curve(qtrace):
     pass
 
 
-def test_set_model_curves(qtrace: TraceDisplay):
+def test_set_model_curves(qtrace):
     pass
 
 
-def test_alter_curve_data(qtrace: TraceDisplay):
+def test_alter_curve_data(qtrace):
     pass
 
 
-def test_convert_curve_to_formula(qtrace: TraceDisplay):
+def test_convert_curve_to_formula(qtrace):
     pass
 
 
-def test_convert_formula_to_curve(qtrace: TraceDisplay):
+def test_convert_formula_to_curve(qtrace):
     pass
 
 
-def test_invalid_formula(qtrace: TraceDisplay):
+def test_invalid_formula(qtrace):
     pass
 
 
-def test_recursive_formula(qtrace: TraceDisplay):
+def test_recursive_formula(qtrace):
     pass
 
 
-def test_next_header(qtrace: TraceDisplay):
+def test_next_header(qtrace):
     pass
 
 
-def test_live_connection_status(qtrace: TraceDisplay):
+def test_live_connection_status(qtrace):
     pass
 
 
-def test_archive_connection_status(qtrace: TraceDisplay):
+def test_archive_connection_status(qtrace):
     pass
 
 
-def test_set_axis_from_unit(qtrace: TraceDisplay):
+def test_set_axis_from_unit(qtrace):
     """Check that when a curve's units change, the curve is moved to an axis
     that reflects the new units.
     """
