2e1adb556cb680ae14cb3800ddefc724243c3de4
Zach Domke <zdomke@slac.stanford.edu>
2024-10-25 16:07:32 -0700

    TST: Add more axis model tests

--- trace/tests/test_table_models/test_axis_model.py
+++ trace/tests/test_table_models/test_axis_model.py
@@ -1,5 +1,6 @@
 from json import loads
 
+import pytest
 from qtpy.QtCore import Qt
 
 
@@ -84,6 +85,7 @@ def test_remove_axis(qtrace):
     """
     axis_model = qtrace.axis_table_model
     axis_model.removeAtIndex(axis_model.index(0, 0))
+
     assert axis_model.rowCount() == 1
     assert axis_model.data(axis_model.index(0, 0)) == "Axis 1"
 
@@ -100,7 +102,8 @@ def test_remove_axis(qtrace):
 
 
 def test_set_model_axes(qtrace, get_test_file):
-    """
+    """Test that setting ArchiverAxisModel.set_model_axes() actually sets the
+    model with the expected y-axes
 
     Parameters
     ----------
@@ -111,14 +114,61 @@ def test_set_model_axes(qtrace, get_test_file):
 
     Expectations
     ------------
+    Calling ArchiverAxisModel.set_model_axes() results in the model containing
+    the set y-axis data
     """
     test_filename = get_test_file("test_file.trc")
     test_data = loads(test_filename.read_text())
 
-    qtrace.axis_table_model.set_model_axes(test_data["y-axes"])
+    axis_model = qtrace.axis_table_model
+    axis_model.set_model_axes(test_data["y-axes"])
 
-    assert False
+    plot_data = qtrace.main_plot.getYAxes()
+    for axis_test, axis_json in zip(test_data["y-axes"], plot_data):
+        axis_actual = loads(axis_json)
+        assert axis_test.items() <= axis_actual.items()
 
 
-def test_alter_axis_data(qtrace):
-    pass
+@pytest.mark.parametrize(
+    ["column", "data_test", "data_expected"],
+    [
+        (0, "FOO Axis", "FOO Axis"),
+        (1, "right", "Right"),
+        (2, "FOO Label", "FOO Label"),
+        (3, -15.0, -15.0),
+        (3, 20.0, 1.04),
+        (4, -15.0, -1.04),
+        (4, 20.0, 20.0),
+        (5, Qt.Checked, Qt.Checked),
+        (5, Qt.Unchecked, Qt.Unchecked),
+        (6, Qt.Checked, Qt.Checked),
+        (6, Qt.Unchecked, Qt.Unchecked),
+    ],
+)
+def test_alter_axis_data(qtrace, column, data_test, data_expected):
+    """Test that users can set data in the ArchiverAxisModel
+
+    Parameters
+    ----------
+    qtrace : fixture
+        Instance of TraceDisplay for application testing
+    column : int
+        The column to set the test data in
+    data_test : Any
+        The test data to set for a given column
+    data_expected : Any
+        The expected value to be stored in the model for the given index
+
+    Expectations
+    ------------
+    The given column and test data should result in the given expected data
+    """
+    axis_model = qtrace.axis_table_model
+
+    index = axis_model.index(0, column)
+    role = Qt.CheckStateRole if column in axis_model.checkable_col else Qt.EditRole
+
+    axis_model.setData(axis_model.index(0, column), data_test, role)
+    data_actual = index.data(role)
+
+    assert data_expected == data_actual
