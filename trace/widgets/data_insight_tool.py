53614116682ce5aaceddd1791ace42a2453bc442
Zach Domke <zdomke@slac.stanford.edu>
2024-12-04 16:59:02 -0800

    ENH: Add live data to Data Insight Tool and DIT to Trace

--- trace/widgets/data_insight_tool.py
+++ trace/widgets/data_insight_tool.py
@@ -1,21 +1,25 @@
 import os
 import re
-import sys
 import json
 import logging
+from typing import Iterable
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timezone
 
+import numpy as np
 import pandas as pd
+import tzlocal
+from epics import caget
 from scipy.io import savemat
 from qtpy.QtCore import (
     Qt,
     QUrl,
+    Slot,
     Signal,
     QObject,
     QModelIndex,
-    QStringListModel,
     QAbstractTableModel,
+    QSortFilterProxyModel,
 )
 from qtpy.QtNetwork import QNetworkReply, QNetworkRequest, QNetworkAccessManager
 from qtpy.QtWidgets import (
@@ -27,12 +31,17 @@ from qtpy.QtWidgets import (
     QMessageBox,
     QPushButton,
     QVBoxLayout,
-    QApplication,
 )
 
-# from widgets import FrozenTableView
-from frozen_table_view import FrozenTableView
+from pydm.widgets.archiver_time_plot import (
+    TimePlotCurveItem,
+    ArchivePlotCurveItem,
+    PyDMArchiverTimePlot,
+)
 
+from widgets import FrozenTableView
+
+TZ = tzlocal.get_localzone()
 SEVERITY_MAP = {0: "NO_ALARM", 1: "MINOR", 2: "MAJOR", 3: "INVALID"}
 
 logger = logging.getLogger("")
@@ -45,12 +54,15 @@ if not logger.hasHandlers():
     handler.setLevel("DEBUG")
 
 
-class ArchiveDataModel(QAbstractTableModel):
+class DataVisualizationModel(QAbstractTableModel):
     reply_recieved = Signal()
 
     def __init__(self, parent: QObject = None) -> None:
         super().__init__(parent)
-        self.df = pd.DataFrame(columns=["Datetime", "Value", "Severity", "Unit", "Source"])
+        self.df = pd.DataFrame(columns=["Datetime", "Value", "Severity", "Source"])
+
+        self.unit = None
+        self.description = None
 
         self.network_manager = QNetworkAccessManager()
         self.network_manager.finished.connect(self.recieve_archive_reply)
@@ -72,6 +84,44 @@ class ArchiveDataModel(QAbstractTableModel):
     def headerData(self, section: int, orientation: Qt.Orientation, role: Qt.ItemDataRole = Qt.DisplayRole):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self.df.columns[section]
+
+    def set_all_data(self, curve_item: TimePlotCurveItem, x_range: Iterable[int]):
+        curve_range = (curve_item.min_x(), curve_item.max_x())
+        left_ts = max(x_range[0], curve_range[0])
+        right_ts = min(x_range[1], curve_range[1])
+
+        # Populate the model with live data if it is shown on the plot
+        if (curve_range[0] <= x_range[1]) and (x_range[0] <= curve_range[1]):
+            self.set_live_data(curve_item, (left_ts, right_ts))
+
+        # Populate the model with archive data if it is shown on the plot
+        if x_range[0] <= curve_range[0]:
+            from_dt = datetime.fromtimestamp(x_range[0], tz=timezone.utc)
+            to_dt = datetime.fromtimestamp(left_ts, tz=timezone.utc)
+            self.request_archive_data(curve_item.address, from_dt, to_dt)
+        else:
+            self.reply_recieved.emit()
+
+        self.unit = curve_item.units
+        self.description = caget(curve_item.address + ".DESC")
+
+    def set_live_data(self, curve_item: TimePlotCurveItem, ts_range: Iterable[int]):
+        data_n = curve_item.getBufferSize()
+        data = curve_item.data_buffer[:, :data_n]
+        indices = np.where((ts_range[0] <= data[0]) & (data[0] <= ts_range[1]))[0]
+
+        convert_data = {"Datetime": [], "Value": [], "Severity": []}
+        convert_data["Datetime"] = data[0, indices]
+        convert_data["Value"] = data[1, indices]
+        convert_data["Severity"] = ["NaN"] * indices.size
+        convert_data["Source"] = ["Live"] * indices.size
+
+        live_df = pd.DataFrame(convert_data)
+        live_df["Datetime"] = live_df["Datetime"].apply(datetime.fromtimestamp)
+
+        self.beginResetModel()
+        self.df = live_df
+        self.endResetModel()
 
     def request_archive_data(self, pv_name: str, from_dt: datetime, to_dt: datetime):
         from_date_str = from_dt.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
@@ -95,7 +145,7 @@ class ArchiveDataModel(QAbstractTableModel):
         if reply.error() == QNetworkReply.NoError:
             bytes_str = reply.readAll()
             data_dict = json.loads(str(bytes_str, "utf-8"))
-            self.set_all_data(data_dict)
+            self.set_archive_data(data_dict)
         else:
             logger.debug(
                 f"Request for data from archiver failed, request url: {reply.url()} retrieved header: "
@@ -103,22 +153,24 @@ class ArchiveDataModel(QAbstractTableModel):
             )
         reply.deleteLater()
 
-    def set_all_data(self, data_dict: dict):
+    def set_archive_data(self, data_dict: dict):
         convert_data = {"Datetime": [], "Value": [], "Severity": []}
         for point in data_dict[0]["data"]:
             ts = point["secs"] + (point["nanos"] * 1e-9)
             convert_data["Datetime"].append(datetime.fromtimestamp(ts))
             convert_data["Value"].append(point["val"])
             convert_data["Severity"].append(SEVERITY_MAP[point["severity"]])
-        try:
-            convert_data["Unit"] = [data_dict[0]["meta"]["EGU"]] * len(data_dict[0]["data"])
-        except KeyError:
-            logger.debug("Requested data has no unit.")
         convert_data["Source"] = ["Archive"] * len(data_dict[0]["data"])
+        archive_df = pd.DataFrame(convert_data)
 
-        self.beginResetModel()
-        self.df = pd.DataFrame(data=convert_data)
-        self.endResetModel()
+        if self.df.empty:
+            self.beginResetModel()
+            self.df = archive_df
+            self.endResetModel()
+        else:
+            self.beginInsertRows(QModelIndex(), 0, archive_df.shape[0] - 1)
+            self.df = pd.concat([archive_df, self.df])
+            self.endInsertRows()
         self.layoutChanged.emit()
 
     def export_data(self, file_name: Path, extension: str):
@@ -143,70 +195,67 @@ class ArchiveDataModel(QAbstractTableModel):
         self.df["Datetime"] = backup
 
 
+class CurveFilterModel(QSortFilterProxyModel):
+    def filterAcceptsRow(self, source_row: int, source_parent: QModelIndex):
+        curve = self.sourceModel().curve_at_index(source_row)
+        return isinstance(curve, ArchivePlotCurveItem) and bool(curve.address)
+
+
 class DataInsightTool(QWidget):
-    # def __init__(self, curve_model: QAbstractTableModel, parent: QObject = None) -> None:
-    def __init__(self, parent: QObject = None) -> None:
+    def __init__(self, parent: QObject, curves_model: QAbstractTableModel, plot: PyDMArchiverTimePlot) -> None:
         super().__init__(parent=parent)
         self.setWindowFlag(Qt.Window)
         self.resize(600, 600)
         self.setWindowTitle("Data Insight Tool")
 
-        if parent:
-            # Get curves model and plot
-            pass
-        else:
-            # Only for testing. Make dummy curves model and plot
-            self.curves_model = QStringListModel(
-                ["KLYS:LI22:31:KVAC", "SBST:LI29:1:AMPL", "LASR:IN20:196:PWR", "TPG:SYS0:1:MANUAL_PATH"]
-            )
+        # Get curves model and function for getting plot x-range
+        self.curves_model = curves_model
+        self.curve_filter_model = CurveFilterModel()
+        self.curve_filter_model.setSourceModel(self.curves_model)
+        self.plot = plot
 
-            def getXRange():
-                return [datetime(2024, 11, 17), datetime(2024, 11, 20)]
+        self.layout_init()
 
-            self.get_time_range = getXRange
-
-        self.init()
-
-        self.archive_data_model.reply_recieved.connect(self.loading_label.hide)
-        self.request_button.clicked.connect(self.get_data)
+        self.data_vis_model.reply_recieved.connect(self.loading_label.hide)
         self.export_button.clicked.connect(self.export_data_to_file)
+        self.pv_select_box.currentIndexChanged.connect(self.get_data)
+        self.refresh_button.clicked.connect(self.get_data)
 
-    def init(self):
+        self.get_data(0)
+
+    def layout_init(self):
         self.main_layout = QVBoxLayout()
 
         # Populate the PV selection and request layout at the top of the widget
         self.request_layout = QHBoxLayout()
         self.pv_select_box = QComboBox()
-        self.pv_select_box.setModel(self.curves_model)
+        self.pv_select_box.setSizeAdjustPolicy(QComboBox.AdjustToContents)
+        self.pv_select_box.setModel(self.curve_filter_model)
         self.request_layout.addWidget(self.pv_select_box, alignment=Qt.AlignLeft)
 
         self.loading_label = QLabel("Loading...")
         self.loading_label.hide()
         self.request_layout.addWidget(self.loading_label, alignment=Qt.AlignCenter)
 
-        self.request_button = QPushButton("Request Data")
-        self.request_layout.addWidget(self.request_button, alignment=Qt.AlignRight)
+        self.export_button = QPushButton("Export to File")
+        self.request_layout.addWidget(self.export_button, alignment=Qt.AlignRight)
         self.main_layout.addLayout(self.request_layout)
 
+        # Create the metadata label and refresh button
+        self.metadata_layout = QHBoxLayout()
+        self.metadata_label = QLabel()
+        self.metadata_layout.addWidget(self.metadata_label, alignment=Qt.AlignLeft)
+
+        self.refresh_button = QPushButton("Refresh Data")
+        self.metadata_layout.addWidget(self.refresh_button, alignment=Qt.AlignRight)
+        self.main_layout.addLayout(self.metadata_layout)
+
         # Set up the main data table in the center of the widget
-        self.archive_data_model = ArchiveDataModel()
-        self.data_table = FrozenTableView(self.archive_data_model)
+        self.data_vis_model = DataVisualizationModel()
+        self.data_table = FrozenTableView(self.data_vis_model)
         self.main_layout.addWidget(self.data_table)
 
-        # Set up the export data layout at the bottom of the widget
-        self.export_layout = QHBoxLayout()
-        self.export_button = QPushButton("Export to File")
-        self.export_layout.addWidget(self.export_button, alignment=Qt.AlignRight)
-        self.main_layout.addLayout(self.export_layout)
-
         self.setLayout(self.main_layout)
-
-    def get_data(self):
-        pv_name = self.pv_select_box.currentText()
-        time_range = self.get_time_range()
-
-        self.archive_data_model.request_archive_data(pv_name, *time_range)
-        self.loading_label.show()
 
     def export_data_to_file(self):
         file_name, extension_filter = QFileDialog.getSaveFileName(
@@ -219,15 +268,26 @@ class DataInsightTool(QWidget):
         file_name = Path(file_name).with_suffix(extension)
 
         try:
-            self.archive_data_model.export_data(file_name, extension)
+            self.data_vis_model.export_data(file_name, extension)
         except (ValueError, IsADirectoryError) as e:
             logger.error(str(e))
             QMessageBox.critical(self, "Error", str(e))
 
+    def set_metadata(self):
+        meta_str = ""
+        if self.data_vis_model.unit:
+            meta_str = self.data_vis_model.unit + ", "
+        meta_str += self.data_vis_model.description
+        self.metadata_label.setText(meta_str)
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    dit = DataInsightTool()
-    dit.show()
+    @Slot()
+    @Slot(int)
+    def get_data(self, curve_index: int = -1):
+        if curve_index < 0:
+            curve_index = self.pv_select_box.currentIndex()
+        curve_item = self.curves_model.curve_at_index(curve_index)
+        x_range = self.plot.getXAxis().range
 
-    app.exec_()
+        self.data_vis_model.set_all_data(curve_item, x_range)
+        self.set_metadata()
+        self.loading_label.show()
