abec22557bcd327bfab9a0e2f5cd0b8ec974c78b
Zach Domke <zdomke@slac.stanford.edu>
2025-03-31 10:33:25 -0700

    ENH: Enable plot's extension lines

--- trace/main.py
+++ trace/main.py
@@ -58,6 +58,8 @@ class TraceDisplay(Display, TracesTableMixin, AxisTableMixin, FileIOMixin, PlotC
         multi_axis_plot.vb.menu = None
         multi_axis_plot.sigXRangeChangedManually.connect(self.ui.cursor_scale_btn.toggle)
 
+        self.ui.main_plot.show_extension_lines = True
+
         # Parse macros & arguments, then include them in startup
         input_file, startup_pvs = self.parse_macros_and_args(macros, args)
         if input_file:
