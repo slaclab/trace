ca8a4a4f32a09bcd2303454aa7e91f322228c34e
Zach Domke <zdomke@slac.stanford.edu>
2025-04-16 16:33:36 -0700

    ENH: Add Y-Axis Gridlines

--- trace/widgets/axis_settings.py
+++ trace/widgets/axis_settings.py
@@ -2,6 +2,7 @@ from qtpy.QtGui import QFont
 from qtpy.QtCore import Qt, Slot
 from qtpy.QtWidgets import (
     QLabel,
+    QSlider,
     QWidget,
     QCheckBox,
     QComboBox,
@@ -55,6 +56,39 @@ class AxisSettingsModal(QWidget):
         log_layout.addWidget(log_checkbox)
         main_layout.addLayout(log_layout)
 
+        grid_layout = QHBoxLayout()
+        grid_label = QLabel("Gridline", self)
+        grid_layout.addWidget(grid_label)
+        grid_spacer = QSpacerItem(40, 12, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        grid_layout.addSpacerItem(grid_spacer)
+        self.grid_checkbox = QCheckBox(self)
+        self.grid_checkbox.stateChanged.connect(self.show_grid)
+        grid_layout.addWidget(self.grid_checkbox)
+        main_layout.addLayout(grid_layout)
+
+        grid_opacity_layout = QHBoxLayout()
+        grid_opacity_label = QLabel("Gridline Opacity", self)
+        grid_opacity_layout.addWidget(grid_opacity_label)
+        grid_opacity_spacer = QSpacerItem(40, 12, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        grid_opacity_layout.addSpacerItem(grid_opacity_spacer)
+        self.grid_opacity_slider = QSlider(self)
+        self.grid_opacity_slider.setOrientation(Qt.Horizontal)
+        self.grid_opacity_slider.setValue(50)
+        self.grid_opacity_slider.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.grid_opacity_slider.valueChanged.connect(self.change_gridline_opacity)
+        grid_opacity_layout.addWidget(self.grid_opacity_slider)
+        main_layout.addLayout(grid_opacity_layout)
+
+    @property
+    def grid_visible(self):
+        return self.grid_checkbox.isChecked()
+
+    @property
+    def gridline_opacity(self):
+        opacity = self.grid_opacity_slider.value()
+        opacity /= 100
+        return opacity
+
     def show(self):
         parent_pos = self.parent().rect().bottomRight()
         global_pos = self.parent().mapToGlobal(parent_pos)
@@ -73,3 +107,18 @@ class AxisSettingsModal(QWidget):
     @Slot(int)
     def set_axis_log_mode(self, checked: int):
         self.axis.log_mode = bool(checked)
+
+    @Slot(bool)
+    @Slot(int)
+    def show_grid(self, visible: bool | int):
+        if not visible:
+            self.axis.setGrid(False)
+        else:
+            self.axis.setGrid(self.gridline_opacity)
+
+    @Slot(int)
+    def change_gridline_opacity(self, opacity: int):
+        if not self.grid_visible:
+            return
+        opacity /= 100
+        self.axis.setGrid(opacity)
