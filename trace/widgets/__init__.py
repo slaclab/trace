9a2f3a3e595bf320703b4c117f13fc1e4b45fffb
Zach Domke <zdomke@slac.stanford.edu>
2025-04-16 14:55:47 -0700

    ENH: Add Axis Modal to widgets/__init__.py

--- trace/widgets/__init__.py
+++ trace/widgets/__init__.py
@@ -12,4 +12,5 @@ from .frozen_table_view import FrozenTableView
 from .data_insight_tool import DataInsightTool
 from .settings_components import SettingsTitle, SettingsRowItem, ComboBoxWrapper
 from .plot_settings import PlotSettingsModal
+from .axis_settings import AxisSettingsModal
 from .curve_settings import CurveSettingsModal
